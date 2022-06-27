75e4c00ff5889b4ac2a31e04a73245c01a54318d
Alena Zharina <alenazharina@coherentsolutions.com>
2022-06-27 13:42:38 +0300

    New work

---
+++ SecondProject/Program1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SecondProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarray = { 1,2,3,4,5,6};
+
+            for (int index = myarray.Length - 1; index >= 0; index--)
+            {
+                Console.WriteLine(myarray [index]);
+            }
+
+            
+        }
+    }
+}
